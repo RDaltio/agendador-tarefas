9141414b751fbf828535acb9fbc55926fadf06cc
Rafael Duarte Daltio <m320000@alunos.ipb.pt>
2024-12-13 14:14:01 +0000

    adicionando métodos GET por email e por período

--- src/main/java/com/rdaltio/agendadortarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/rdaltio/agendadortarefas/business/mapper/TarefasConverter.java
@@ -3,10 +3,20 @@ package com.rdaltio.agendadortarefas.business.mapper;
 import com.rdaltio.agendadortarefas.business.dto.TarefasDTO;
 import com.rdaltio.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "dataEvento", target = "dataEvento")
+    @Mapping(source = "dataCriacao", target = "dataCriacao")
 
     TarefasEntity paraTarefasEntity(TarefasDTO dto);
+
     TarefasDTO paraTarefasDTO(TarefasEntity entity);
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefasDTO> dtos);
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
 }
